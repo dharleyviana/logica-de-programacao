622b8e55bfa6cdbe8265dbd9c57be36b4b4ad906
Dharley Viana <dharleyfvkd@gmail.com>
2022-08-11 02:18:30 -0300

    sim

--- controle/switch_1.js
+++ controle/switch_1.js
@@ -1,19 +1,18 @@
-let nota = 12.4;
+let nota = 5.4;
 
 switch (Math.ceil(nota)) {
+  case 8:
+    console.log("Parabéns!");
+    break;
+  case 3:
+    console.log("Reprovado!");
+    break
+  case 2:
+    console.log("Muito Reprovado")
   default:
     console.log("Nota Inválida")
     break
-  case 10: case 9: case 8: case 7:
-    console.log('Parabens');
-    break
-  case 6:
-  case 5:
-    console.log("Passou")
-    break;
-  case 4:
-  case 3:
-  case 2:
-    console.log("Reprovado");
+
 }
-console.log("fim!");
+
+console.log("Fim!")
