9b96d53dc589941187f6f9d8bf5f78ed848291a9
Dharley Viana <dharleyfvkd@gmail.com>
2022-08-22 05:27:15 -0300

    oi

---
+++ Funções/com_funcao_2.js
@@ -0,0 +1,13 @@
+function datas(d, m, a) {
+  console.log(`${d}/${m}/${a}`);
+}
+
+datas(23,12,2017);
+
+
+function horas(h, m, s) {
+ console.log(`São: ${h} horas ${m} minutos e ${s} segundos`);
+}
+
+horas(2, 33, 23);
+horas(13, 10, 41);
